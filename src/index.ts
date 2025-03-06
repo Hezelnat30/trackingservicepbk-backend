aada6f81041d4c4557afe2c14838a7527661cf2a
Hezelnat30 <justin.hezekiel123@gmail.com>
2025-03-06 21:52:14 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+async function init() {
+  app.use("/", (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "Server is running!",
+      data: null,
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+  });
+}
+
+init();
